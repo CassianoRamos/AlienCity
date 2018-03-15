0ece652181d22a1f29d7597c25251861b262f0d5
CassianoRamos <30438171+CassianoRamos@users.noreply.github.com>
2018-03-14 21:13:18 -0300

    Programação de tiro inicial
    Adição de animação de tiro e correção das animações de movimento do player

---
+++ Assets/Scripts/Tiro.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiro : MonoBehaviour {
+
+	// Use this for initialization
+	private Animator anim;
+	private Rigidbody2D rb2d;
+	public float Forcatiro = 1000f;
+	public bool tiro;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("j")) {
+			tiro = true;
+		}
+	}
+	void FixedUpdate(){
+	
+		if (tiro == true) {
+			anim.SetTrigger ("tiro");
+			tiro = false;
+		}
+
+		else { anim.SetTrigger("sem tiro");
+			
+		}
+
+	}
+  }
+
